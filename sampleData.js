c65fce19751a67800a4767baf805ec33a6e67b86
CalebAckom <calebackom@amalitech.com>
2023-01-31 16:45:51 +0000

    Testing GraphiQL

---
+++ sampleData.js
@@ -0,0 +1,79 @@
+// Projects
+const projects = [
+    {
+        id: '1',
+        clientId: '1',
+        name: 'eCommerce Website',
+        description:
+            'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu.',
+        status: 'In Progress',
+    },
+    {
+        id: '2',
+        clientId: '2',
+        name: 'Performance Monitoring',
+        description:
+            'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu.',
+        status: 'In Progress',
+    },
+    {
+        id: '3',
+        clientId: '3',
+        name: 'CV Builder',
+        description:
+            'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu.',
+        status: 'In Progress',
+    },
+    {
+        id: '4',
+        clientId: '4',
+        name: 'Automotive App',
+        description:
+            'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu.',
+        status: 'Done',
+    },
+    {
+        id: '5',
+        clientId: '5',
+        name: 'QR Code',
+        description:
+            'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu.',
+        status: 'In Progress',
+    },
+];
+
+// Clients
+const clients = [
+    {
+        id: '1',
+        name: 'Caleb Ackom',
+        email: 'test@mail.com',
+        phone: '123-456-7890',
+    },
+    {
+        id: '2',
+        name: 'Jabez Ackom',
+        email: 'test@mail.com',
+        phone: '123-456-7890',
+    },
+    {
+        id: '3',
+        name: 'Philemon Ackom',
+        email: 'test@mail.com',
+        phone: '123-456-7890',
+    },
+    {
+        id: '4',
+        name: 'Justina Ackom',
+        email: 'test@mail.com',
+        phone: '123-456-7890',
+    },
+    {
+        id: '5',
+        name: 'Joyce Ackom',
+        email: 'test@mail.com',
+        phone: '123-456-7890',
+    },
+];
+
+module.exports = { projects, clients };
